71d768ed9035e33a463ed3dbdf5904735240f03e
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-10-13 18:04:19 -0400

    Clase Carta de Modelo creada

---
+++ CartasWeb/CartasWeb/Models/Carta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartasWeb.Models
+{
+    public class Carta
+    {
+        [Key]
+        public string NaipeId { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre de la carta")]
+        public string Nombre { get; set; }
+
+
+        [Required]
+        [Url]
+        [Display(Name = "Link de la imagen")]
+        public string Imagen { get; set; }
+    }
+}
