40b4ba3962b4f12678f74c6168cf21e6da8e91b5
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-10-13 18:12:01 -0400

    AppDbContextCreado

---
+++ CartasWeb/CartasWeb/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CartasWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartasWeb.Data
+{
+    public class AppDbContext:DbContext
+    { 
+        public DbSet<Carta>Cartas { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
